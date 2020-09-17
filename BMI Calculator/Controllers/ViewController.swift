ece5641c97fc865a2cafaa66f4ad00f4a176d8d9
Zeduo Zhang <jarviszhangzeduo@gmail.com>
2020-09-17 05:13:52 -0400

    final update

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var bmiValue = "0.0"
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
 
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value)+"m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value))+"kg"
+    }
+    @IBAction func calculateBMI(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height,2)
+        bmiValue = String(format: "%.1f", bmi)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue,sender: Any?) {
+        if (segue.identifier == "goToResult") {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
+    }
 }
 
